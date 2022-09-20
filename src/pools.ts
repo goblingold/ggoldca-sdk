a9b84e07a04f20e147a8b90c9aae0de6361cae9d
skrrb <skrrb@pm.me>
2022-09-20 19:54:11 +0200

    added wh pools

--- src/pools.ts
+++ src/pools.ts
@@ -1,8 +1,12 @@
 export enum Pools {
+  BTC_USDC = "ErSQss3jrqDpQoLEYvo6onzjsi6zm4Sjpoz1pjqz2o6D",
+  mSOL_USDC = "AiMZS5U3JMvpdvsr1KeaMiS354Z1DeSg5XjA4yYRxtFf",
   SOL_mSOL = "HQcY5n2zP6rW74fyFEhWeBd3LnJpBcZechkvJpmdb8cx",
+  SOL_USDC = "HJPjoWUrhoZzkNfRpHuieeFk9WcZWjwy6PBjZ81ngndJ",
   USDC_USDT = "4fuUiYxTQ6QCrdSq9ouBYcTM7bqSwYTSyLueGZLTy4T4",
   USDH_USDC = "Fvtf8VCjnkqbETA6KtyHYqHm26ut6w184Jqm4MQjPvv7",
   USH_USDC = "ApLVWYdXzjoDhBHeRx6SnbFWv4MYjFMih5FijDQUJk5R",
   USH_USDT = "3K92dMW5CNKa6ShbJtWHicBQrMHujWLGwHWDHWJvqesi",
   UXD_USDC = "GLNvG5Ly4cK512oQeJqnwLftwfoPZ4skyDwZWzxorYQ9",
+  whETH_USDC = "FYjYk3KMQeSrwg1NfLoAmPSq1crD2yhTSWskAsmC6WFF",
 }
