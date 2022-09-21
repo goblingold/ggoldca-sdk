ae4cae725eb6b4624945dcf4e5b04add9effde0d
skrrb <skrrb@pm.me>
2022-09-21 15:49:44 +0200

    fix whETH_USDC pool

--- src/pools.ts
+++ src/pools.ts
@@ -8,5 +8,5 @@ export enum Pools {
   USH_USDC = "ApLVWYdXzjoDhBHeRx6SnbFWv4MYjFMih5FijDQUJk5R",
   USH_USDT = "3K92dMW5CNKa6ShbJtWHicBQrMHujWLGwHWDHWJvqesi",
   UXD_USDC = "GLNvG5Ly4cK512oQeJqnwLftwfoPZ4skyDwZWzxorYQ9",
-  whETH_USDC = "FYjYk3KMQeSrwg1NfLoAmPSq1crD2yhTSWskAsmC6WFF",
+  whETH_USDC = "E5KuHFnU2VuuZFKeghbTLazgxeni4dhQ7URE4oBtJju2",
 }
